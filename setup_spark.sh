31d929ab98e60088adc61aaadae4219fd1c9b141
Feng Li <m@feng.li>
2025-02-23 20:03:43 +0800

    Update Python and pyspark environment

--- setup_spark.sh
+++ setup_spark.sh
@@ -25,29 +25,34 @@ miniforge_home=$HOME/.local/miniforge3
 # Install minforge if mamba not available
 if [[ ! -f ${miniforge_home}/bin/python3 ]]; then
 
-    # Force download the json file
+    # Force download the miniforge file
     curl -L -O "https://mirrors.tuna.tsinghua.edu.cn/github-release/conda-forge/miniforge/LatestRelease/Miniforge3-$(uname)-$(uname -m).sh"
     bash "Miniforge3-$(uname)-$(uname -m).sh" -b -u -p "${miniforge_home}"
 
-#    ln -sfv ${miniforge_home}/bin/mamba ${HOME}/.local/bin/
+    #  ln -sfv ${miniforge_home}/bin/mamba ${HOME}/.local/bin/
 fi
 
+# Install Python with specific version
+# ${miniforge_home}/bin/mamba create -n python$python3_ver python=$python3_ver --yes
+# conda activate pyspark_env
 
-# Install dependency
+# Install dependency at the base environment
 ${miniforge_home}/bin/mamba install openjdk=17                      --yes
 ${miniforge_home}/bin/mamba install pyspark="$spark_ver" plotly     --yes
 ${miniforge_home}/bin/mamba install ipykernel                       --yes
 
-# Make pyspark availale for jupyter
+# Make pyspark available for Jupyter
 mkdir -p ${HOME}/.local/share/jupyter/kernels/pyspark
 
-# Set JAVA_HOME dynamically (Detect Java 11 installation)
+# Set JAVA_HOME
 JAVA_HOME=${miniforge_home}
 PYSPARK_PYTHON=${miniforge_home}/bin/python
-SPARK_HOME=${miniforge_home}
 
-# Ensure the kernel directory exists
-$PYSPARK_PYTHON -m ipykernel install --user --name pyspark --display-name pyspark
+# PYSPARK_PYTHON=${miniforge_home}/envs/python$python3_ver/bin/python
+# SPARK_HOME=${miniforge_home} # Do not set this for standardalone
+
+# Ensure the kernel directory exists using base python
+${miniforge_home}/bin/python -m ipykernel install --user --name pyspark --display-name pyspark
 
 # mkdir -p ${HOME}/.local/share/jupyter/kernels/pyspark
 # Write the CORRECT JSON content with the dynamic JAVA_HOME
@@ -68,4 +73,14 @@ cat > ${HOME}/.local/share/jupyter/kernels/pyspark/kernel.json <<EOF
 }
 EOF
 
-echo "✅ Pyspark kernel written successfully with JAVA_HOME: $JAVA_HOME"
+echo -e "
+✅ PySpark, JDK and Jupyter kernel installed to the conda base environment successfully.
+
+To stark PySpark from a terminal, run:
+
+        PYSPARK_PYTHON=$PYSPARK_PYTHON ${miniforge_home}/bin/pyspark
+        
+To run PySpark within Jupyter Notebook, run:
+
+        jupyter notebook
+"
